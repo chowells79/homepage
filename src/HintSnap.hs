41389a06891c60e42c520b01fa3a8f349a65d3db
Carl Howells <chowells@others.local>
2010-06-15 11:03:18 -0700

    make HintSnap work correctly

--- src/HintSnap.hs
+++ src/HintSnap.hs
@@ -1,8 +1,17 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, OverloadedStrings #-}
 module HintSnap where
 
-import Data.ByteString.Char8 ( pack )
+import Data.ByteString.Char8 ( length, pack )
 
+import Control.Concurrent ( forkIO )
+import Control.Concurrent.MVar
+    ( MVar
+    , newMVar
+    , newEmptyMVar
+    , takeMVar
+    , putMVar
+    )
+import Control.Monad ( when )
 import Control.Monad.Trans ( liftIO )
 
 import Data.Typeable ( Typeable )
@@ -19,9 +28,17 @@ import Language.Haskell.Interpreter
 
 import Language.Haskell.Interpreter.Unsafe ( unsafeSetGhcOption )
 
-import Prelude hiding ( init )
+import Prelude hiding ( init, length )
 
-import Snap.Types ( Snap, writeBS )
+import Snap.Types
+    ( Snap
+    , modifyResponse
+    , setContentLength
+    , setContentType
+    , setHeader
+    , setResponseStatus
+    , writeBS
+    )
 
 loadSnap :: forall a. (Typeable a) => String -> [String] -> String ->
             IO a -> IO (Snap ())
@@ -33,10 +50,38 @@ loadSnap sPath mNames aName init = do
         setImports [ "Site", "Snap.Internal.Types", "Text.Templating.Heist.Types" ]
         interpret aName (undefined :: a -> Snap ())
 
-  state <- init
+  readInterpreter <- multiReader $ runInterpreter interpreter
 
   return $ do
-    eSnap <- liftIO . runInterpreter $ interpreter
+    interpreterMVar <- liftIO $ readInterpreter
+    eSnap <- liftIO . takeMVar $ interpreterMVar
     case eSnap of
-      Left err -> writeBS . pack . show $ err
-      Right makeSnap -> makeSnap state
+      Left e -> do
+          let err = pack . show $ e
+
+          modifyResponse $ setContentType "text/plain; charset=utf-8"
+                         . setResponseStatus 500 "Internal Server Error"
+                         . setHeader "X-Internal-Error" err
+                         . setContentLength (fromIntegral $ length err)
+
+          writeBS err
+
+      Right makeSnap -> liftIO init >>= makeSnap
+
+multiReader :: IO a -> IO (IO (MVar a))
+multiReader action = do
+  readerContainer <- newMVar []
+  return $ do
+    reader <- newEmptyMVar
+    readers <- takeMVar readerContainer
+
+    when (null readers) $ do
+      forkIO $ do
+        result <- action
+        readers' <- takeMVar readerContainer
+        putMVar readerContainer []
+        mapM_ (flip putMVar result) readers'
+      return ()
+
+    putMVar readerContainer $ reader : readers
+    return reader
