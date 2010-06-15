3d3feefa9a69ceb4fe0418c1e8441a945d7a8498
Carl Howells <chowells@others.local>
2010-06-15 11:02:52 -0700

    Handle reverse DNS lookup failure

--- src/Site.hs
+++ src/Site.hs
@@ -5,10 +5,10 @@ import Control.Applicative ( (<$>) )
 import Control.Monad ( msum )
 import Control.Monad.Trans ( liftIO )
 
-import Data.ByteString.Char8 ( length, pack, unpack )
+import Data.ByteString.Char8 ( ByteString, length, pack, unpack )
 
 import Network.BSD ( getHostByAddr , hostName)
-import Network.Socket ( Family(AF_INET), inet_addr )
+import Network.Socket ( HostAddress, Family(AF_INET), inet_addr )
 
 import Prelude hiding ( length )
 
@@ -21,7 +21,7 @@ import Snap.Types
     , modifyResponse
     , rqRemoteAddr
     , setContentLength
-    , setHeader
+    , setContentType
     , writeBS
     )
 
@@ -36,16 +36,13 @@ import Text.Templating.Heist
 
 frontPage :: TemplateState Snap -> Snap ()
 frontPage ts = ifTop $ do
-                 modifyResponse $ setHeader "Content-Type"
-                                    "text/html; charset=utf-8"
+                 modifyResponse $ setContentType "text/html; charset=utf-8"
 
                  let docType = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\r\n"
                  writeBS docType
 
                  ip <- rqRemoteAddr <$> getRequest
-                 hostAddr <- liftIO . inet_addr $ unpack ip
-                 let hostAct = getHostByAddr AF_INET hostAddr
-                 hName <- pack . hostName <$> liftIO hostAct
+                 hName <- liftIO $ safeHostFromAddr ip
 
                  let ts' = bindStrings [ ("ip", ip), ("rdns", hName) ] ts
 
@@ -54,6 +51,13 @@ frontPage ts = ifTop $ do
                  let len = length docType + length rendered
                  modifyResponse . setContentLength . fromIntegral $ len
 
+
+safeHostFromAddr :: ByteString -> IO ByteString
+safeHostFromAddr ip = resolve `catch` \e -> return ip
+    where resolve = do
+            hostAddr <- inet_addr $ unpack ip
+            host <- getHostByAddr AF_INET hostAddr
+            return . pack . hostName $ host
 
 staticResources :: Snap ()
 staticResources = fileServe "resources/static"
