876772b2d318f71c766eddca4ec8d937f3ae4414
Carl Howells <chowells@others.local>
2010-06-18 15:06:14 -0700

    Add minimum wait between recompiles in HintSnap

--- src/HintSnap.hs
+++ src/HintSnap.hs
@@ -16,14 +16,17 @@ import Data.List ( nub )
 
 import Control.Concurrent ( forkIO )
 import Control.Concurrent.MVar
-    ( MVar
-    , newMVar
+    ( newMVar
     , newEmptyMVar
-    , takeMVar
     , putMVar
+    , readMVar
+    , swapMVar
+    , takeMVar
     )
 import Control.Monad ( when )
 import Control.Monad.Trans ( liftIO )
+
+import Data.Time.Clock ( NominalDiffTime, diffUTCTime, getCurrentTime )
 
 import Language.Haskell.Interpreter
     ( InterpreterError(..)
@@ -54,29 +57,27 @@ import Snap.Types
 -- Control.Monad.Trans
 loadSnap :: String -> [String] -> String -> IO (Snap ())
 loadSnap sPath mNames action = do
-  let interpreter = do
+    let interpreter = do
         unsafeSetGhcOption "-hide-package=mtl"
         set [ searchPath := [sPath] ]
         loadModules . nub $ mNames
-        let allMods = "Prelude" : "Snap.Types" : "Control.Monad.Trans" : mNames
+        let allMods = "Prelude":"Snap.Types":"Control.Monad.Trans":mNames
         setImports . nub $ allMods
         interpret action (as :: Snap ())
 
-  readInterpreter <- multiReader $ runInterpreter interpreter
+    loadAction <- protectedActionEvaluator 3 $ runInterpreter interpreter
 
-  return $ do
-    eSnap <- liftIO $ readInterpreter >>= takeMVar
-    case eSnap of
-      Left e -> do
-          let err = format e
+    return $ do
+        eSnap <- liftIO loadAction
+        case eSnap of
+            Left err -> do
+                let msg = format err
+                modifyResponse $ setContentType "text/plain; charset=utf-8"
+                               . setResponseStatus 500 "Internal Server Error"
+                               . setContentLength (fromIntegral $ length msg)
+                writeBS msg
 
-          modifyResponse $ setContentType "text/plain; charset=utf-8"
-                         . setResponseStatus 500 "Internal Server Error"
-                         . setContentLength (fromIntegral $ length err)
-
-          writeBS err
-
-      Right handler -> handler
+            Right handler -> handler
 
 
 format :: InterpreterError -> ByteString
@@ -94,20 +95,29 @@ format (WontCompile errs) =
     in append "Compile errors:\r\n\r\n" formatted
 
 
-multiReader :: IO a -> IO (IO (MVar a))
-multiReader action = do
-  readerContainer <- newMVar []
-  return $ do
-    reader <- newEmptyMVar
-    readers <- takeMVar readerContainer
+protectedActionEvaluator :: NominalDiffTime -> IO a -> IO (IO a)
+protectedActionEvaluator minReEval action = do
+    readerContainer <- newMVar []
+    resultContainer <- newMVar Nothing
+    return $ do
+        existingResult <- readMVar resultContainer
+        now <- getCurrentTime
 
-    when (null readers) $ do
-      forkIO $ do
-        result <- action
-        readers' <- takeMVar readerContainer
-        putMVar readerContainer []
-        mapM_ (flip putMVar result) readers'
-      return ()
+        case existingResult of
+            Just (val, ts) | diffUTCTime now ts < minReEval -> return val
+            _ -> do
+                reader <- newEmptyMVar
+                readers <- takeMVar readerContainer
 
-    putMVar readerContainer $ reader : readers
-    return reader
+                when (null readers) $ do
+                    forkIO $ do
+                        result <- action
+                        readers' <- takeMVar readerContainer
+                        finishTime <- getCurrentTime
+                        swapMVar resultContainer $ Just (result, finishTime)
+                        putMVar readerContainer []
+                        mapM_ (flip putMVar result) readers'
+                    return ()
+
+                putMVar readerContainer $ reader : readers
+                takeMVar reader
