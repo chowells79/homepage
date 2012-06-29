bbbe62c63f3735704da7b9ffd1b175a32b328fbd
Carl Howells <chowells79@gmail.com>
2012-06-29 22:51:41 +0000

    update to newer ghc and snap

--- src/Site.hs
+++ src/Site.hs
@@ -5,65 +5,69 @@ import Control.Applicative ( (<$>) )
 import Control.Monad ( msum )
 import Control.Monad.Trans ( liftIO )
 
-import Data.ByteString.Char8 ( ByteString, length, pack, unpack )
+import Control.Exception ( SomeException, catch )
+
+import Data.ByteString.Char8 ( ByteString, pack, unpack )
+import Data.Monoid
+import qualified Data.Text as T
 
 import Network.BSD ( getHostByAddr , hostName)
 import Network.Socket ( Family(AF_INET), inet_addr )
 
-import Prelude hiding ( length )
+import Prelude hiding ( catch )
 
-import Snap.Types
+import Snap.Core
     ( Snap
     , getRequest
     , ifTop
     , ipHeaderFilter'
     , modifyResponse
     , rqRemoteAddr
-    , setContentLength
     , setContentType
-    , writeBS
+    , writeBuilder
     )
 
-import Snap.Util.FileServe ( fileServe )
+import Snap.Util.FileServe ( serveDirectory )
 
 import Text.Templating.Heist
-    ( TemplateState
+    ( HeistState
     , bindStrings
     , renderTemplate
-    , emptyTemplateState
     , loadTemplates
     )
 
 
-frontPage :: TemplateState Snap -> Snap ()
+frontPage :: HeistState Snap -> Snap ()
 frontPage ts = ifTop $ do
-                 modifyResponse $ setContentType "text/html; charset=utf-8"
-
                  ip <- rqRemoteAddr <$> getRequest
                  hName <- liftIO $ safeHostFromIp ip
 
-                 let ts' = bindStrings [ ("ip", ip), ("rdns", hName) ] ts
+                 let ts' = bindStrings [ ("ip", t ip), ("rdns", t hName) ] ts
+                     t = T.pack . unpack
 
-                 Just rendered <- renderTemplate ts' "index"
-                 writeBS rendered
-                 let len = fromIntegral . length $ rendered
-                 modifyResponse . setContentLength $ len
+                 Just (rendered, mimeType) <- renderTemplate ts' "index"
+                 writeBuilder rendered
+                 modifyResponse $ setContentType mimeType
 
 
 safeHostFromIp :: ByteString -> IO ByteString
-safeHostFromIp ip = resolve `catch` \_ -> return ip
-    where resolve = do
-            hostAddr <- inet_addr $ unpack ip
-            host <- getHostByAddr AF_INET hostAddr
-            return . pack . hostName $ host
+safeHostFromIp ip = resolve `catch` unchanged
+  where
+    unchanged :: SomeException -> IO ByteString
+    unchanged _ = return ip
+
+    resolve = do
+        hostAddr <- inet_addr $ unpack ip
+        host <- getHostByAddr AF_INET hostAddr
+        return . pack . hostName $ host
 
 
 staticResources :: Snap ()
-staticResources = fileServe "resources/static"
+staticResources = serveDirectory "resources/static"
 
 
-site :: TemplateState Snap -> Snap ()
-site ts = do
+makeSite :: HeistState Snap -> Snap ()
+makeSite ts = do
   ipHeaderFilter' "x-real-ip"
 
   msum [ frontPage ts
@@ -71,8 +75,7 @@ site ts = do
        ]
 
 
-setup :: IO (TemplateState Snap)
+setup :: IO (HeistState Snap)
 setup = do
-  let ets = loadTemplates "resources/templates"
-            (emptyTemplateState :: TemplateState Snap)
+  let ets = loadTemplates "resources/templates" (mempty :: HeistState Snap)
   either error id <$> ets
